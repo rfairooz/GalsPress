e22816d3f38fb28cad202d02445ba61d90b6183c
Raidah Fairooz <rfair008@ucr.edu>
2024-01-20 13:12:20 -0800

    merging dev assets

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Textarea from '@/components/Textarea';
+import Image from 'next/image';
+import woman from '../public/assets/landingWoman.svg';
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className="h-screen w-screen bg-gradient-to-r from-[#53A1FB]/50 via-[#81A0FA] to-[#9A93E1]/90 ">
+        <Image src={woman}/>
+        <Textarea/>
+    </div>
   )
 }
 
