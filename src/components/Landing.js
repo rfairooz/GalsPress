ff9a16dd69f929f4e524c53216538e75ece21bc6
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-01-20 18:21:50 -0800

    ui update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,14 +9,15 @@ const Landing = () => {
 
 
       <Image className="w-full absolute" src={bubbles} />
-
-      <div className="text-8xl font-bold z-10 text-[#0E1836] text-center relative">
+<div className='flex justify-center'>
+      <div className="border-3 w-2/5 border-[#450315] text-8xl font-bold z-10 text-[#450315]/80 text-center relative mt-4">
         GALS PRESS
+      </div>
       </div>
       <hr className="text-[#C63B61] z-10" />
 
-      <div className="grid grid-cols-2 mt-20 relative">
-        <div className="grid relative">
+      <div className="grid grid-cols-2 items-center mt-14 relative">
+        <div className="grid relative ml-20 ">
           <Textarea />
           {/* <Image className="w-full" src={woman} /> */}
         </div>
